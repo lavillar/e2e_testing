fad18d12c5cad1143ba93da851b0680e4c35f27d
LuisVE <LuisVE@inditex.com>
2021-03-11 20:05:09 +0100

    Esqueleto from start.spring.io

---
+++ src/main/java/lve/lab/e2e_testing/E2eTestingApplication.java
@@ -0,0 +1,13 @@
+package lve.lab.e2e_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2eTestingApplication.class, args);
+	}
+
+}
